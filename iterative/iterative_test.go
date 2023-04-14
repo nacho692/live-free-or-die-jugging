8fff289d983bdcc8901357d2e114e954a4e6d3e6
ignaciomariotti <ignacio.mariotti@brubank.com>
2023-04-14 00:27:01 -0300

    Add testify

--- iterative/iterative_test.go
+++ iterative/iterative_test.go
@@ -1,6 +1,8 @@
 package iterative_test
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/nacho692/live-free-or-die-jugging/iterative"
@@ -10,70 +12,94 @@ import (
 func TestNoSolution(t *testing.T) {
 
 	_, err := iterative.Solve(newBaseState(9, 3), 4)
-	if err == nil {
-		t.Error("error expected")
-	}
-	if err != models.ErrNoSolution {
-		t.Error("no solution error expected")
-	}
+
+	assert.ErrorIs(t, err, models.ErrNoSolution)
 }
 
 func TestInvalid(t *testing.T) {
 
 	t.Run("x should be positive", func(t *testing.T) {
 		_, err := iterative.Solve(newBaseState(-5, 3), 4)
-		if err == nil {
-			t.Errorf("error expected")
-		}
+		assert.Error(t, err)
 	})
 
 	t.Run("y should be positive", func(t *testing.T) {
 		_, err := iterative.Solve(newBaseState(5, -3), 4)
-		if err == nil {
-			t.Errorf("error expected")
-		}
+		assert.Error(t, err)
 	})
 
 	t.Run("z should be zero or greater", func(t *testing.T) {
 		_, err := iterative.Solve(newBaseState(5, 3), -4)
-		if err == nil {
-			t.Errorf("error expected")
-		}
+		assert.Error(t, err)
 	})
 
 	t.Run("z should be lower than either x or y", func(t *testing.T) {
 		_, err := iterative.Solve(newBaseState(5, 3), 10)
-		if err == nil {
-			t.Errorf("error expected")
-		}
+		assert.Error(t, err)
 	})
 }
 
 func TestSolutions(t *testing.T) {
 
 	t.Run("simple solution", func(t *testing.T) {
-		s, err := iterative.Solve(newBaseState(5, 3), 4)
-		if err != nil {
-			t.Fatalf("error %s not expected", err.Error())
+		solution, err := iterative.Solve(newBaseState(5, 3), 4)
+		require.NoError(t, err)
+
+		expectedSolution := models.Solution{
+			Steps: []models.Step{
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 5},
+						Y: models.Jug{Capacity: 3, Amount: 0},
+					},
+					Action: models.ActionFillX,
+				},
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 2},
+						Y: models.Jug{Capacity: 3, Amount: 3},
+					},
+					Action: models.ActionTransferY,
+				},
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 2},
+						Y: models.Jug{Capacity: 3, Amount: 0},
+					},
+					Action: models.ActionEmptyY,
+				},
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 0},
+						Y: models.Jug{Capacity: 3, Amount: 2},
+					},
+					Action: models.ActionTransferY,
+				},
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 5},
+						Y: models.Jug{Capacity: 3, Amount: 2},
+					},
+					Action: models.ActionFillX,
+				},
+				{
+					State: models.State{
+						X: models.Jug{Capacity: 5, Amount: 4},
+						Y: models.Jug{Capacity: 3, Amount: 3},
+					},
+					Action: models.ActionTransferY,
+				},
+			},
 		}
-		if len(s.Steps) != 6 {
-			t.Error("6 steps expected")
-		}
-		if s.Steps[len(s.Steps)-1].State.X.Amount != 4 &&
-			s.Steps[len(s.Steps)-1].State.Y.Amount != 4 {
-			t.Errorf("solution not found")
-		}
+
+		assert.Equal(t, expectedSolution, solution)
 	})
 
 	t.Run("z = 0 should be measurable in 0 steps", func(t *testing.T) {
 
 		s, err := iterative.Solve(newBaseState(5, 3), 0)
-		if err != nil {
-			t.Fatalf("error %s not expected", err.Error())
-		}
-		if len(s.Steps) != 0 {
-			t.Error("0 steps expected")
-		}
+		require.NoError(t, err)
+		assert.Len(t, s.Steps, 0)
 	})
 }
 
